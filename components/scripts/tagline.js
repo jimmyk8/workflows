671dbd12c49e6979102867dc6c4b9d5d44ddf51e
jimmyK <graphart8@hotmail.com>
2018-03-04 15:24:54 -0600

    gulpif adds syntax error to .coffee/ tagline.js file

--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -3,7 +3,7 @@ var $, fill;
 $ = require('jquery');
 
 (fill = function(item) {
-  return $('.tagline').append(`${item}`);
+  return $('.tagline').append("" + item);
 })('Good thing they live with their parents');
 
 fill;
