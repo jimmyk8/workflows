e402cdf67801f6f563e1687126867310877d68be
jimmyK <graphart8@hotmail.com>
2018-03-04 13:56:42 -0600

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var jsSources = [
 	];
 
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -47,13 +49,25 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
 	connect.server({
 		root: 'builds/development/',
 		livereload: true
-	})
+	});
+});
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
 })
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('json', function() {
+	gulp.src('builds/development/js/*.json')
+	.pipe(connect.reload())
+})
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'html', 'json', 'watch']);
